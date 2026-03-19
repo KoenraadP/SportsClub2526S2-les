b0cd9d0f572bce2f9658f35a7fa95e4aa8c42d2b
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-03-19 09:31:39 +0100

    1903931

--- SportsClub.Data/Migrations/20260319083056_Validation.Designer.cs
+++ SportsClub.Data/Migrations/20260319083056_Validation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SportsClub.Data;
 
@@ -11,9 +12,11 @@ using SportsClub.Data;
 namespace SportsClub.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260319083056_Validation")]
+    partial class Validation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- SportsClub.Models/Member.cs
+++ SportsClub.Models/Member.cs
@@ -1,16 +1,38 @@
-﻿namespace SportsClub.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SportsClub.Models
 {
     public class Member
     {
         // id is nodig voor databank, duidt specifieke records aan, dit is de 'Primary Key'
         public int MemberId { get; set; }
+
+        [Required(ErrorMessage = "Voornaam moet ingevuld worden!")]
+        [StringLength(50,MinimumLength = 2, ErrorMessage = "Voornaam moet minstens 2 tekens zijn, maximaal 50")]
+        [Display(Name = "Voornaam")]
         public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Achternaam moet ingevuld worden!")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Achternaam moet minstens 2 tekens zijn, maximaal 50")]
+        [Display(Name = "Achternaam")]
         public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "E-mail moet ingevuld worden!")]
+        [EmailAddress(ErrorMessage = "Dit is geen correct e-mail adres!")]
+        [Display(Name = "E-mail")]
         public string Email { get; set; } = string.Empty;
+
         // PhoneNumber is niet verplicht in te vullen, dus kan eventueel null zijn
         // duiden we aan door vraagteken na type te plaatsen
+        [Phone(ErrorMessage = "Dit is geen correct telefoonnummer!")]
+        [Display(Name = "Telefoonnummer")]
         public string? PhoneNumber { get; set; }
-        public DateTime BirthDate { get; set; }         
+
+        [Required(ErrorMessage = "Geboortedatum moet ingevuld worden!")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Geboortedatum")]
+        public DateTime BirthDate { get; set; } 
+        
 
         // relatie property voor lijst van activities
         // want een member kan ingeschreven zijn voor meerdere activities
