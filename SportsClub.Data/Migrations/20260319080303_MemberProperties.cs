40abaf6786d3c062349888d018e4892496075695
Koenraad Pecceu <koenraad.pecceu@gmail.com>
2026-03-19 09:17:48 +0100

    1903917

---
+++ SportsClub.Data/Migrations/20260319080303_MemberProperties.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportsClub.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MemberProperties : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Members",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Members",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "MemberId",
+                keyValue: 1,
+                columns: new[] { "BirthDate", "Email", "PhoneNumber" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "", null });
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "MemberId",
+                keyValue: 2,
+                columns: new[] { "BirthDate", "Email", "PhoneNumber" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "", null });
+
+            migrationBuilder.UpdateData(
+                table: "Members",
+                keyColumn: "MemberId",
+                keyValue: 3,
+                columns: new[] { "BirthDate", "Email", "PhoneNumber" },
+                values: new object[] { new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), "", null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Members");
+        }
+    }
+}
--- SportsClub.Models/Member.cs
+++ SportsClub.Models/Member.cs
@@ -6,6 +6,11 @@
         public int MemberId { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        // PhoneNumber is niet verplicht in te vullen, dus kan eventueel null zijn
+        // duiden we aan door vraagteken na type te plaatsen
+        public string? PhoneNumber { get; set; }
+        public DateTime BirthDate { get; set; }         
 
         // relatie property voor lijst van activities
         // want een member kan ingeschreven zijn voor meerdere activities
@@ -19,10 +24,14 @@
         }
 
         // constructor met de nodige eigenschappen
-        public Member(string firstName, string lastName)
+        public Member(string firstName, string lastName,
+            string email, string? phoneNumber, DateTime birthDate)
         {
             FirstName = firstName;
             LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            BirthDate = birthDate;
         }
     }
 }
